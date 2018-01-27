278a914e893a27b5858174b09ed5766f99091bd9
Anna Sosna <annasosna94@gmail.com>
2018-01-28 00:03:09 +0100

    end

--- db_connection.py
+++ db_connection.py
@@ -8,54 +8,50 @@ Created on Fri Jan 19 21:13:06 2018
 
 import pyodbc
 
-#conn = pyodbc.connect(
-#    r'DRIVER={ODBC Driver 13 for SQL Server};'
-#    r'SERVER=ud-project.database.windows.net;'
-#    r'DATABASE=UD_Project;'
-#    r'UID=ud_admin@ud-project;'
-#    r'PWD=aoud_project!1311'
-#    )
+server = 'ud-project.database.windows.net' 
+database = 'UD_Project' 
+username = 'ud_admin@ud-project' 
+password = 'aoud_project!1311' 
 
 conn = pyodbc.connect(
-    r'DRIVER={Devart ODBC Driver for PostgreSQL};'
-    r'SERVER=localhost;'
-    r'DATABASE=Aoud;'
-    r'UID=postgres;'
-    r'PORT=5432;'
-    r'PWD=sosna3894'
+    r'DRIVER={ODBC Driver 13 for SQL Server};'
+    r'SERVER='+server+';'
+    r'DATABASE='+database+';'
+    r'UID='+username+';'
+    r'PWD='+password, autocommit = True
     )
 
 cursor = conn.cursor()
 
 
-#create_table_main = """
-#    CREATE TABLE MAIN(
-#            ID int NOT NULL PRIMARY KEY,
-#            LINK VARCHAR(300),
-#            LOCATION VARCHAR(20),
-#            LAT VARCHAR(20),
-#            LONG VARCHAR(20),
-#            COMPANY VARCHAR(100),
-#            TITLE VARCHAR(300),
-#            EMPLOY_TYPE VARCHAR(200),
-#            DATE_BEGIN DATE,
-#            DATE_END DATE,
-#            DATE_VALID DATE,
-#            SALARY VARCHAR(50),
-#            SPONSORED BOOLEAN,
-#            LANGUAGE CHAR(2),
-#            KEYWORDS VARCHAR(400),
-#            SNIPPET VARCHAR(400),
-#            COMPANY_DES VARCHAR(2000),
-#            OFFER_DES VARCHAR(2000))
-#    """
-#    
-#    
-#create_table_query = textwrap.dedent("""
-#    CREATE TABLE QUERY(
-#            ID INT REFERENCES MAIN(ID),
-#            QUERY VARCHAR (150))
-#    """)
+create_table_main = """
+    CREATE TABLE MAIN(
+            ID int NOT NULL PRIMARY KEY,
+            LINK VARCHAR(300),
+            LOCATION VARCHAR(20),
+            LAT VARCHAR(20),
+            LONG VARCHAR(20),
+            COMPANY VARCHAR(100),
+            TITLE VARCHAR(300),
+            EMPLOY_TYPE VARCHAR(200),
+            DATE_BEGIN DATE,
+            DATE_END DATE,
+            DATE_VALID DATE,
+            SALARY VARCHAR(50),
+            SPONSORED BIT,
+            LANGUAGE CHAR(2),
+            KEYWORDS VARCHAR(400),
+            OFFER_DES VARCHAR(8000))
+    """
+    
+create_table_query = """
+    CREATE TABLE QUERY(
+            ID INT,
+            QUERY VARCHAR (150),
+            PRIMARY KEY (ID, QUERY),
+            CONSTRAINT FK_QueryID FOREIGN KEY (ID)
+            REFERENCES MAIN(ID))
+    """
     
 create_table_new_offers = """
     CREATE TABLE NEW_OFFERS(
@@ -71,21 +67,16 @@ create_table_new_offers = """
             DATE_BEGIN DATE,
             DATE_VALID DATE,
             SALARY VARCHAR(50),
-            SPONSORED BOOLEAN,
+            SPONSORED BIT,
             LANGUAGE CHAR(2),
             KEYWORDS VARCHAR(400),
-            SNIPPET VARCHAR(400),
-            COMPANY_DES VARCHAR(2000),
-            OFFER_DES VARCHAR(2000))
+            OFFER_DES VARCHAR(8000))
     """
     
-#cursor.execute(create_table_main)
-#conn.commit()
-#cursor.execute(create_table_query)
-#conn.commit()
-
+cursor.execute(create_table_main)
+cursor.execute(create_table_query)
 cursor.execute(create_table_new_offers)
-#cursor.execute('DROP TABLE NEW_OFFERS')
-conn.commit()
+
+
 
 
--- html_parsing.py
+++ html_parsing.py
@@ -5,19 +5,16 @@ Created on Wed Nov  8 08:28:01 2017
 @author: Ania
 """
 
-import datetime 
 import getopt
 import os
 from glob import glob
-import string
 import sys
-import re
-import logging
 import os.path
 import urllib.request
 from bs4 import BeautifulSoup as BS
-import math
-
+from langdetect import detect
+import pyodbc 
+import datetime
 
 
 def main():
@@ -56,26 +53,27 @@ def main():
         except:
             Next_page = False
 
+
 def parse_html(soup,query,location):
     soup=soup.find_all('li', class_='o-list_item')
     for i in soup:
             id=i.find(class_='o-list_item_link_name').get('data-gtm-offer')
             link="https://www.pracuj.pl"+i.find(class_='o-list_item_link_name').get('href')
             company=i.find(class_='o-list_item_link_emp o-list_item_link_emp--hover seolink').string
-            data=i.find(itemprop="datePosted").string
             if i.find(class_='label superOfferLabel'):
-                sponsored='true'
+                sponsored= True
             else:
-                sponsored='false'
-            snippet=i.find(class_="o-list_item_text_details clearfix acc_cnt").string
+                sponsored= False 
             job_title=i.find(class_="o-list_item_link_name").string
             salary=i.find(class_="label salaryLabel")
             if salary:
                 salary=salary.string
             else:
                 salary=''
-            get_data(link)
-                
+            keywords,lat,long,employ_type,date_posted,validThrough,offer_description,language = get_data(link)
+            if language=='en':
+                save_to_database(id,link,location,lat,long,query,company,job_title,employ_type,date_posted,validThrough,salary,sponsored,language,keywords,offer_description)
+            
             
 def get_data(link):
     try:
@@ -88,14 +86,50 @@ def get_data(link):
     adress = soup.find('span', attrs={'class':'o-main__right_offer_cnt_details_item_text ico-location latlng'})
     lat = adress.get('data-lat')
     long = adress.get('data-lng')
-    employ_type = soup.find('span', attrs={'itemprop':'employmentType'}).text
-    date_posted = soup.find('span', attrs={'itemprop':'datePosted'}).string
-    validThrough = soup.find('span', attrs={'itemprop':'validThrough'}).string
-    company_description = soup.find('div', attrs={'id':'company'}).text
-    offer_description = soup.find('div', attrs={'id':'description'}).text
+    employ_type = soup.find('span', attrs={'itemprop':'employmentType'}).text.replace('\n',' ')
+    date_posted = soup.find('span', attrs={'itemprop':'datePosted'}).text
+    if '.' in date_posted:
+        date_posted = datetime.datetime.strptime(date_posted, "%d.%m.%Y" )
+    else:
+        date_posted = datetime.datetime.strptime(date_posted, "%Y-%m-%d" )
+    validThrough = soup.find('span', attrs={'itemprop':'validThrough'}).string.replace(')','')
+    if '.' in validThrough:
+        validThrough = datetime.datetime.strptime(validThrough, "%d.%m.%Y" )
+    else:
+        validThrough = datetime.datetime.strptime(validThrough, "%Y-%m-%d" )
+    offer_description = soup.find('div', attrs={'id':'description'})
+    if offer_description:
+        offer_description = offer_description.text
+    else:
+        offer_description = soup.find('div', attrs={'class':'o-main__left_table uploaded'}).text
+    try:
+        language = detect(offer_description)
+    except:
+        language = '0'
+    return keywords,lat,long,employ_type,date_posted,validThrough,offer_description,language
+
     
+def save_to_database(id,link,location,lat,long,query,company,job_title,employ_type,date_posted,validThrough,salary,sponsored,language,keywords,offer_description):
     
+    server = 'ud-project.database.windows.net' 
+    database = 'UD_Project' 
+    username = 'ud_admin@ud-project' 
+    password = 'aoud_project!1311' 
     
+    conn = pyodbc.connect(
+    r'DRIVER={ODBC Driver 13 for SQL Server};'
+    r'SERVER='+server+';'
+    r'DATABASE='+database+';'
+    r'UID='+username+';'
+    r'PWD='+password, autocommit = True
+    )
+    
+    cursor = conn.cursor()
+    insert_data_sql = '''INSERT into NEW_OFFERS VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) '''
+    try:
+        cursor.execute(insert_data_sql,(id,link,location,lat,long,query,company,job_title,employ_type,date_posted,validThrough,salary,sponsored,language,keywords,offer_description))    
+    except:
+        pass
     
 if __name__ == "__main__":
     main()
