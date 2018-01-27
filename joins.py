9d723902c7073456375fdefaad60d1cc01517ae1
Anna Sosna <annasosna94@gmail.com>
2018-01-27 23:42:55 +0100

    first full commit

---
+++ joins.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 27 20:17:19 2018
+
+@author: Ania
+"""
+
+import pyodbc
+import datetime
+
+server = 'ud-project.database.windows.net' 
+database = 'UD_Project' 
+username = 'ud_admin@ud-project' 
+password = 'aoud_project!1311' 
+
+conn = pyodbc.connect(
+    r'DRIVER={ODBC Driver 13 for SQL Server};'
+    r'SERVER='+server+';'
+    r'DATABASE='+database+';'
+    r'UID='+username+';'
+    r'PWD='+password, autocommit = True
+    )
+
+
+cursor = conn.cursor()
+
+
+sql_end_date = """ update public.MAIN 
+    set DATE_END = ?
+    where public.MAIN.id not in (
+            select id  
+            from public.new_offers) 
+    and DATE_END is null """
+
+cursor.execute(sql_end_date,datetime.datetime.now())
+
+sql_update_query = """ INSERT INTO QUERY (id,query)
+                        SELECT id,query FROM NEW_OFFERS
+                        LEFT JOIN QUERY 
+                        ON NEW_OFFERS.ID=QUERY.ID
+                        AND
+                        NEW_OFFERS.QUERY=QUERY.QUERY """
+                        
+cursor.execute(sql_update_query)
+
+select_data_sql = 'SELECT * FROM NEW_OFFERS LEFT JOIN MAIN ON NEW_OFFERS.ID=MAIN.ID'
+cursor.execute(select_data_sql)
+sql_insert= """ INSERT INTO MAIN (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) """
+for row in cursor.fetchall():
+    cursor.execute(sql_insert,(row.ID,row.LINK,row.LOCATION,row.LAT,row.LONG,row.COMPANY,row.TITLE, row.EMPLOY_TYPE, row.DATE_BEGIN, None, row.DATE_VALID, row.SALARY, row.SPONSORED, row.LANGUAGE, row.KEYWORDS, row.OFFER_DES))
